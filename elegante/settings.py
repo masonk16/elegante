aa63bea1cafd115f77f934d84e42353e55e5b050
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-04-04 19:53:14 +0200

    Initialize cart app

--- elegante/settings.py
+++ elegante/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shop.apps.ShopConfig",
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CART_SESSION_ID = 'cart'
