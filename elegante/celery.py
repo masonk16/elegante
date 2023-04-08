ae0a29f03051d3c3b12316988a0377830d87bdbf
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-04-08 22:19:20 +0200

    Setup tasks and monitoring

--- elegante/celery.py
+++ elegante/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elegante.settings')
 
-app = Celery('elegante')
+app = Celery('elegante', broker='amqp://guest:**@localhost:5672//')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
--- elegante/settings.py
+++ elegante/settings.py
@@ -140,3 +140,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 CART_SESSION_ID = 'cart'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
