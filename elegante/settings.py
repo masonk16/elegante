6baf97a9e258ee2a9bec34268155f3a135aa86af
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-04-11 20:58:21 +0200

    Add cpoupon application form to cart detail template

--- elegante/settings.py
+++ elegante/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     # '/var/www/static/',
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # '/var/www/static/',
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media settings
 MEDIA_URL = "/media/"
