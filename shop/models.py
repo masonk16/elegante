3f17fd94506f06098d0b3476150c7dfcd50911f1
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-04-18 21:57:19 +0200

    Translating model fields with django-parler

--- shop/models.py
+++ shop/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.urls import reverse
+from parler.models import TranslatableModel, TranslatedFields
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, unique=True)
+class Category(TranslatableModel):
+    translations = TranslatedFields(
+        name=models.CharField(max_length=200, db_index=True),
+        slug=models.SlugField(max_length=200, unique=True),
+    )
 
     class Meta:
-        ordering = ("name",)
+        # ordering = ("name",)
         verbose_name = "category"
         verbose_name_plural = "categories"
 
@@ -18,24 +21,26 @@ class Category(models.Model):
         return reverse("shop:product_list_by_category", args=[self.slug])
 
 
-class Product(models.Model):
+class Product(TranslatableModel):
+    translations = TranslatedFields(
+        name=models.CharField(max_length=200, db_index=True),
+        slug=models.SlugField(max_length=200, db_index=True),
+        description=models.TextField(blank=True),
+    )
     sku = models.CharField(max_length=20, unique=True, null=True)
     category = models.ForeignKey(
         Category, related_name="products", on_delete=models.CASCADE
     )
     brand = models.CharField(max_length=30, null=True)
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
-    description = models.TextField(blank=True)
     specification = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        ordering = ("name",)
-        index_together = (("id", "slug"),)
+    # class Meta:
+    #     ordering = ("name",)
+    #     index_together = (("id", "slug"),)
 
     def __str__(self):
         return self.name
