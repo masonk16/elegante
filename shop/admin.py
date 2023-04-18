ef426fd23ce90d7462acbff20ce52010ec7ebfde
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-04-18 22:13:42 +0200

    migrations for model translations

--- shop/admin.py
+++ shop/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-
-
+from parler.admin import TranslatableAdmin
 from .models import Category, Product, ProductImage
 
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TranslatableAdmin):
     list_display = ["name", "slug"]
-    prepopulated_fields = {"slug": ("name",)}
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
 
 
 class ProductImageInline(admin.TabularInline):
@@ -15,12 +16,14 @@ class ProductImageInline(admin.TabularInline):
 
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(TranslatableAdmin):
     list_display = ["name", "brand", "slug", "price", "available", "created", "updated"]
     list_filter = ["available", "created", "updated"]
     list_editable = ["price", "available"]
-    prepopulated_fields = {"slug": ("name",)}
     inlines = [ProductImageInline]
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
 
 
 
