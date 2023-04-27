8f5416bf5ee3efff44cbd115f9e17c8f391711ca
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-04-27 22:55:54 +0200

    Empty cart zunionstore error handling

--- cart/views.py
+++ cart/views.py
@@ -37,12 +37,11 @@ def cart_detail(request):
             initial={"quantity": item["quantity"], "override": True}
         )
     coupon_apply_form = CouponApplyForm()
-
+    context = {"cart": cart, "coupon_apply_form": coupon_apply_form}
     r = Recommender()
     cart_products = [item['product'] for item in cart]
-    recommended_products = r.suggest_products_for(
-        cart_products, max_results=4)
-    return render(request, "cart/cart-detail.html",
-                  {"cart": cart,
-                   "coupon_apply_form": coupon_apply_form,
-                   "recommended_products": recommended_products})
+    if len(cart_products) >= 1:
+        recommended_products = r.suggest_products_for(
+            cart_products, max_results=4)
+        context["recommended_products"] = recommended_products
+    return render(request, "cart/cart-detail.html", context)
--- orders/views.py
+++ orders/views.py
@@ -25,6 +25,7 @@ def order_create(request):
             if cart.coupon:
                 order.coupon = cart.coupon
                 order.discount = cart.coupon.discount
+                del request.session['coupon_id']
             order.save()
             for item in cart:
                 OrderItem.objects.create(
